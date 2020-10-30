79b0a38c4674a0d6699dc9208803d4924d5c89be
huangwanqing0810 <www.86108115@qq.com>
2020-10-30 20:36:55 +0800

    head

--- meituan1/src/views/home/Head.vue
+++ meituan1/src/views/home/Head.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-我是首页的头
+    <div class="container">
+        <input type="text" placeholder="请输入商品">
     </div>
 </template>
 
@@ -11,5 +11,19 @@
 </script>
 
 <style lang="scss" scoped>
+.container{
+    background-color:#1c1b20 ;
+    text-align: center;
+    padding:0.2rem 0;
+    input{
+        width:90%;
+        height: 0.6rem;
+        outline: none;
+        border:none;
+        border-radius: 0.3rem;
+        text-align: center;
+    }
+    
+}
 
 </style>
--- meituan1/src/views/home/index.vue
+++ meituan1/src/views/home/index.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
-我是首页
+        <!-- Head组件 -->
+        <Head></Head>
+
+        <Category></Category>
+
+        <List></List>
     </div>
 </template>
 
-<script>
+<script>/*
+    1.先import引入
+    2.在components中声明
+    3.在template中使用
+ */
+    import Head from "./Head"
+    import List from "./List"
+    import Category from "./Category"
     export default {
-        
+        components:{
+            Head,
+            Category,
+            List
+        }
     }
+
+
 </script>
 
 <style lang="scss" scoped>
+
 
 </style>
